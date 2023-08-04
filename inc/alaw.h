ba2a3b78a0ad5dec676ba9a758b8ebbc9bfffa5d
David <davidvans.dv@gmail.com>
2023-08-03 21:59:36 -0700

    Add decomp

--- inc/alaw.h
+++ inc/alaw.h
@@ -6,6 +6,9 @@
 #undef TESTING 
 
 int a_law(WAV_Header *header, FILE *input, FILE *output);
+int a_law_decomp(WAV_Header *header, FILE *input, FILE *output);
 int8_t a_law_convert(int16_t val);
+int16_t a_law_unconvert(uint8_t val);
+
 
 #endif
