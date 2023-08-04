7c0ec465dadb996b42bf5376544414e873050cd7
David <davidvans.dv@gmail.com>
2023-08-03 22:03:58 -0700

    Add decomp

--- src/alaw.c
+++ src/alaw.c
@@ -146,3 +146,109 @@ static uint8_t get_sign(int16_t num) {
 		return 0x80;
 }
 
+int a_law_decomp(WAV_Header *header, FILE *input, FILE *output)
+{
+	// Read chunk size (should match header plus size of header and 'data')
+	uint32_t chunks;
+	fread(&chunks, sizeof(chunks), 1, input);
+#ifdef TESTING
+	uint32_t header_chunks = header->size - sizeof(WAV_Header) + 4;
+	if(chunks != header_chunks) {
+		printf("Error: Header (%d) and data (%d) chunk sizes do not match\n", header_chunks, chunks);
+		return 1;
+	}
+	printf("Number of chunks to read: %d\n", chunks);
+
+	// Size of data to be read
+	if(header->bits_per_sample != 8) {
+		printf("Error: Data size is not 16 bits");
+		return 1;
+	}
+#endif
+
+	// Move past header in output file
+	fseek(output, sizeof(WAV_Header) + sizeof(chunks), SEEK_SET);
+
+	uint8_t sample;		// Sample to be converted
+	uint32_t length = 0;	// Total length of data
+	int16_t converted;	// Converted value to be written
+
+	// For every chunk
+	for(int i = 0; i < chunks; i++) {
+		// Read in one sample
+		fread(&sample, 1, 1, input);
+		// Shift over by number of bits needed to get next sample
+		converted = a_law_unconvert(sample);
+		// Write new data to output file
+		fwrite(&converted, 2, 1, output);
+		// Increment number of bytes written
+		length++;
+	}
+
+	// Length is 2* since 16-bit data
+	length *= 2;
+
+	// Create new header for A-Law data
+	WAV_Header output_header = {
+		{ 'R', 'I', 'F', 'F' },		// "RIFF"
+		length,				// Number of chunks
+		{ 'W', 'A', 'V', 'E' },		// "WAVE"
+		{ 'f', 'm', 't', ' ' },	// "fmt "
+		0x00000010,			// # format bytes
+		0x0001,				// A-Law format
+		header->num_channels,		// Number of channels
+		header->sample_rate,		// Sample rate
+		header->sample_rate * header->num_channels, // Byte rate
+		header->num_channels,		// Container size
+		16,				// Bits per sample
+		{ 'd', 'a', 't', 'a' },		// "data"
+	};
+
+#ifdef TESTING
+	// Print out the new header
+	printf("\nOutput header:\n");
+	print_header(&output_header);
+#endif
+
+	// Move back to beginning of file
+	fseek(output, 0, SEEK_SET);
+	// Write the new header
+	fwrite(&output_header, sizeof(WAV_Header), 1, output);
+	// Write the data length
+	fwrite(&length, sizeof(chunks), 1, output);
+
+	return 0;
+}
+
+int16_t a_law_unconvert(uint8_t val)
+{
+	// Undo XOR with 0x55
+    	val = val ^ (0x0055);
+
+	int16_t val_signless, chord, step;
+    	uint16_t result;
+
+	// Get value without sign
+    	val_signless = val & (0x007F);
+
+	// Extract chord
+    	chord = val_signless >> 4;
+	// Extract step
+    	step = val_signless & (0x000F);
+
+	// If the chord was not zero, add leading 1
+	if(chord > 0)
+		step += 16;
+
+	// Result is step data shifted up plus trailing 1 b/c no zero on decomp
+    	result = (step << 4) + 8;
+
+	// Shift by (chord - 1) if chord greater than 1
+	if(chord > 1)
+		result = result << (chord-1);
+
+	// Invert if greater than 127
+	register uint32_t val_long = val;
+	if(val > 127) return result;
+	else return -result;
+}
